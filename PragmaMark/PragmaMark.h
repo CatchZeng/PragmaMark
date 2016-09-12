b0ed642854efed1cca542eb5ba5060223a8588a8
CatchZeng <891793848@qq.com>
2016-09-12 22:37:57 +0800

    add project

---
+++ PragmaMark/PragmaMark.h
@@ -0,0 +1,16 @@
+//
+//  PragmaMark.h
+//  PragmaMark
+//
+//  Created by Administrator on 16/9/12.
+//  Copyright © 2016年 catch. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface PragmaMark : NSObject
+
++ (instancetype)sharedPlugin;
+
+@property (nonatomic, strong, readonly) NSBundle* bundle;
+@end
