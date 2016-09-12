a7ae11ae26a08028daafe9655907c7588e7f9559
CatchZeng <891793848@qq.com>
2016-09-12 22:58:44 +0800

    add quick key

--- PragmaMark/PragmaMark.m
+++ PragmaMark/PragmaMark.m
@@ -14,30 +14,24 @@ static PragmaMark *sharedPlugin;
 
 #pragma mark - Initialization
 
-+ (void)pluginDidLoad:(NSBundle *)plugin
-{
++ (void)pluginDidLoad:(NSBundle *)plugin{
     NSArray *allowedLoaders = [plugin objectForInfoDictionaryKey:@"me.delisa.XcodePluginBase.AllowedLoaders"];
     if ([allowedLoaders containsObject:[[NSBundle mainBundle] bundleIdentifier]]) {
         sharedPlugin = [[self alloc] initWithBundle:plugin];
     }
 }
 
-+ (instancetype)sharedPlugin
-{
++ (instancetype)sharedPlugin{
     return sharedPlugin;
 }
 
-- (id)initWithBundle:(NSBundle *)bundle
-{
+- (id)initWithBundle:(NSBundle *)bundle{
     if (self = [super init]) {
         // reference to plugin's bundle, for resource access
         _bundle = bundle;
         // NSApp may be nil if the plugin is loaded from the xcodebuild command line tool
         if (NSApp && !NSApp.mainMenu) {
-            [[NSNotificationCenter defaultCenter] addObserver:self
-                                                     selector:@selector(applicationDidFinishLaunching:)
-                                                         name:NSApplicationDidFinishLaunchingNotification
-                                                       object:nil];
+            [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidFinishLaunching:) name:NSApplicationDidFinishLaunchingNotification object:nil];
         } else {
             [self initializeAndLog];
         }
@@ -45,14 +39,12 @@ static PragmaMark *sharedPlugin;
     return self;
 }
 
-- (void)applicationDidFinishLaunching:(NSNotification *)notification
-{
+- (void)applicationDidFinishLaunching:(NSNotification *)notification{
     [[NSNotificationCenter defaultCenter] removeObserver:self name:NSApplicationDidFinishLaunchingNotification object:nil];
     [self initializeAndLog];
 }
 
-- (void)initializeAndLog
-{
+- (void)initializeAndLog{
     NSString *name = [self.bundle objectForInfoDictionaryKey:@"CFBundleName"];
     NSString *version = [self.bundle objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
     NSString *status = [self initialize] ? @"loaded successfully" : @"failed to load";
@@ -61,15 +53,13 @@ static PragmaMark *sharedPlugin;
 
 #pragma mark - Implementation
 
-- (BOOL)initialize
-{
-    // Create menu items, initialize UI, etc.
-    // Sample Menu Item:
+- (BOOL)initialize{
     NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
     if (menuItem) {
         [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
-        NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
-        //[actionMenuItem setKeyEquivalentModifierMask:NSAlphaShiftKeyMask | NSControlKeyMask];
+        NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Pragma Mark" action:@selector(insertPragmaMark) keyEquivalent:@"P"];
+        [actionMenuItem setKeyEquivalentModifierMask:NSShiftKeyMask];
+        
         [actionMenuItem setTarget:self];
         [[menuItem submenu] addItem:actionMenuItem];
         return YES;
@@ -78,12 +68,8 @@ static PragmaMark *sharedPlugin;
     }
 }
 
-// Sample Action, for menu item:
-- (void)doMenuAction
-{
-    NSAlert *alert = [[NSAlert alloc] init];
-    [alert setMessageText:@"Hello, World"];
-    [alert runModal];
+- (void)insertPragmaMark{
+    
 }
 
 @end
